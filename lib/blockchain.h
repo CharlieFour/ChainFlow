8cbd299afbf8eaea69e96e48b1cc1e2a855fec64
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-11-26 21:24:26 +0500

    0.3

--- lib/blockchain.h
+++ lib/blockchain.h
@@ -1,1 +1,15 @@
 #include "block.h"
+
+#include <string>
+
+class BlockChain{
+    public:
+    BlockChain();
+    ~BlockChain();
+    void addBlock(Block * block);
+    void printChain();
+    private:
+    std::vector<Block*> chain;
+    Block* getLastBlock();
+    std::string getTimeStamp();
+};
